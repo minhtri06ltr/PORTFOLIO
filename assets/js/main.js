ca9618df8cb1dfe7aa7c398c43f32df64830236a
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-15 12:45:00 +0000

    qualification content

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,23 @@ function toggleSkills() {
     })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
