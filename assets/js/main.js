77ece10e88a24a55ef0aba5752b39c36935138af
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-16 07:47:47 +0000

    review content

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,7 +88,7 @@ modalCloses.forEach((modalClose) => {
             })
         })
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -108,7 +108,26 @@ let swiper = new Swiper('.portfolio__container', {
 
 /*==================== TESTIMONIAL ====================*/
 
-
+let swiperTestimontial = new Swiper('.testimontial__container', {
+   
+    loop: true,
+    grabCursor: true,
+    spaceBetween:48,
+      
+   
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets:true,
+    },
+    breakpoints: {
+        568: {
+         slidesPerView: 2,
+     }  
+   },
+    mousewheel: true,
+    keyboard:true,
+})
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
